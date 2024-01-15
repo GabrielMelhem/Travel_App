4a51400bd019c05e4cf64fc09830f9d34e189054
GabrielMelhem <gabrielmelhem@gmail.com>
2024-01-15 23:40:35 +0100

    fetch Country Data

--- src/pages/country/Country.jsx
+++ src/pages/country/Country.jsx
@@ -1,11 +1,124 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Country = () => {
-  const { countryName } = useParams();
-  return (
-    <div>{countryName}</div>
-  )
-}
+  const params = useParams();
+  const countryName = params.countryName;
 
-export default Country
+  const [countryData, setCountryData] = useState({
+    name: "",
+    official: "",
+    flagImg: "",
+    population: 0,
+    region: "",
+    subregion: "",
+    capital: "",
+    currencies: {},
+    languages: [],
+    borders: [],
+  });
+
+  const url = "https://restcountries.com/v3.1/name/";
+
+  const fetchCountyData = async () => {
+    try {
+      const fetchData = await fetch(`${url}${countryName}`);
+      const res = await fetchData.json();
+      console.log("countryData", res);
+      setCountryData({
+        ...countryData,
+        name: res[0].name.common,
+        official: res[0].name.official,
+        flagImg: res[0].flags.png,
+        population: res[0].population,
+        region: res[0].region,
+        subregion: res[0].subregion,
+        capital: res[0].capital,
+        currencies: res[0].currencies,
+        languages: res[0].languages,
+        borders: res[0].borders,
+      });
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCountyData();
+  }, [countryName]);
+
+  let languages;
+  languages = Object.values(countryData.languages);
+
+  let borders = [];
+  borders = countryData.borders;
+
+  let currencies;
+  currencies = Object.values(countryData.currencies);
+
+  return (
+    <div>
+      <div>
+        <img src={countryData.flagImg} alt="Country Flag" />
+      </div>
+      <div>
+        <h2>{countryName}</h2>
+      </div>
+      <div>
+        <section>
+          <p>
+            Official Name: <span>{countryData.official}</span>
+          </p>
+          <p>
+            Population: <span>{countryData.population}</span>
+          </p>
+          <p>
+            Region: <span>{countryData.region}</span>
+          </p>
+          <p>
+            Sub Region: <span>{countryData.subregion}</span>
+          </p>
+          <p>
+            Capital: <span>{countryData.capital}</span>
+          </p>
+        </section>
+        <section>
+          <p>
+            Currencies:{" "}
+            <span>
+              {currencies?.map(
+                (currency, index) =>
+                  currency.name +
+                  `${index === currencies.length - 1 ? "" : " , "} `
+              )}
+            </span>
+          </p>
+          <p>
+            Languages:{" "}
+            <span>
+              {languages?.map(
+                (language, index) =>
+                  language + `${index === languages.length - 1 ? "" : " , "} `
+              )}
+            </span>
+          </p>
+        </section>
+      </div>
+      <div>
+        <p>
+          Borders:
+          {borders !== undefined &&
+            countryData.borders.map((border, index) => (
+              <span key={index}>{border}</span>
+            ))}
+        </p>
+      </div>
+      <div>
+        <p>Cities:</p>
+      </div>
+    </div>
+  );
+};
+
+export default Country;
