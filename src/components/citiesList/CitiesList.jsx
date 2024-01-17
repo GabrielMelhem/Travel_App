5eba0daf5d30a4011225c12b20f937174e019248
GabrielMelhem <gabrielmelhem@gmail.com>
2024-01-17 22:49:40 +0100

    fetching cities

--- src/components/citiesList/CitiesList.jsx
+++ src/components/citiesList/CitiesList.jsx
@@ -1,55 +1,79 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import axios from "axios";
 
 const CitiesList = ({ countryCode, countryName }) => {
-  const [citiesData, setCitiesData] = useState({
-    data: [],
-  });
+  const [cities, setCities] = useState([{}]);
+  const [responseData, setResponseData]=useState({});
+
   console.log("country Code", countryCode);
   console.log("country Name", countryName);
 
-  const url = "https://countriesnow.space/api/v0.1/countries/population/cities";
+  const url = "https://countriesnow.space/api/v0.1/countries";
 
+  let resData= [];
+  
   const fetchCitiesList = async () => {
     try {
       const fetchData = await fetch(url);
-      const res = await fetchData.json();
-      console.log("res", res);
-      setCitiesData({
-        ...citiesData,
-        data: res.data,
-      });
-      console.log("citiesData", citiesData);
+      const response = await fetchData.json();
+      console.log("response", response);
+      setResponseData(response.data);
+      console.log('responseData',responseData)
     } catch (error) {
       console.log("error", error);
     }
   };
+
   useEffect(() => {
     fetchCitiesList();
   }, [countryName]);
 
-  let cities = [];
-  cities = citiesData.data;
-  console.log("cities", cities);
+  const countriescList =responseData.map((res,index)=>{
+    return (<li key={index}>{res.country}</li>)
+  })
+  
 
-  const filteredCities = cities.filter((city) => {
-    if (city.country.toLowerCase().includes(countryName.toLowerCase())) {
-      return true;
-    } else {
-      return false;
-    }
-  });
-  console.log('filteredCities',filteredCities)
+  //   const url = "https://countriesnow.space/api/v0.1/countries/population/cities";
 
-  const filteredCitiesList = filteredCities.map((filteredCity, index) => {
-    return <li key={index}> {filteredCity.city}</li>;
-  });
+  //   const fetchCitiesList = async () => {
+  //     try {
+  //       const fetchData = await fetch(url);
+  //       const res = await fetchData.json();
+  //       console.log("res", res);
+  //       setCitiesData({
+  //         ...citiesData,
+  //         data: res.data,
+  //       });
+  //       console.log("citiesData", citiesData);
+  //     } catch (error) {
+  //       console.log("error", error);
+  //     }
+  //   };
+  //   useEffect(() => {
+  //     fetchCitiesList();
+  //   }, [countryName]);
+
+  //   let cities = [];
+  //   cities = citiesData.data;
+  //   console.log("cities", cities);
+
+  //   const filteredCities = cities.filter((city) => {
+  //     if (city.country.toLowerCase().includes(countryName.toLowerCase())) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   });
+  //   console.log('filteredCities',filteredCities)
+
+  //   const filteredCitiesList = filteredCities.map((filteredCity, index) => {
+  //     return <li key={index}> {filteredCity.city}</li>;
+  //   });
 
   return (
     <div>
       <h2>List of Cities</h2>
-      {filteredCitiesList}
+      {countriescList}
     </div>
   );
 };
