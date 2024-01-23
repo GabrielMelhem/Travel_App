9faa11ff19a381b07c7aa59a649871ea249e7795
GabrielMelhem <gabrielmelhem@gmail.com>
2024-01-23 22:07:51 +0100

    Register/Login added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useContext, useState } from "react";
+// import { AuthContext } from "../../context/AuthContext";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  // const { login } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    // console.log("email :>> ", email);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    // console.log("password :>> ", password);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("email, password :>> ", email, password);
+    // login(email, password);
+  };
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}> 
+        <input type="text" id="email" onChange={handleEmailChange} />
+        <label htmlFor="email">Email</label>
+        <input type="password" id="password" onChange={handlePasswordChange}/>
+        <label htmlFor="pssword">Password</label>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
